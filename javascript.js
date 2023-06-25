33f9488642c1f568b149b1112681b99ee1021018
Michael Kucukarslan <michaelkucukarslan@gmail.com>
2023-06-25 16:03:40 -0700

    Added pixels using a function
    This allowed for the user to be able to change the resolution of the
    Etch-A-Sketch

--- javascript.js
+++ javascript.js
@@ -1,29 +1,61 @@
 const canvas = document.getElementById("canvas");
-console.log(canvas);
 
-// add pixels using math
 // get size of canvas
 let canvasWidth = canvas.clientWidth;
 let canvasHeight = canvas.clientHeight;
-console.log("Width: " + canvasWidth + " Height: " + canvasHeight);
-// get size of pixels
-let pixelWidth = Math.floor(canvasWidth/16);
-let pixelHeight = Math.floor(canvasHeight/16);
-// calculate number of pixels needed
-let numberOfPixels = 16*16;
-let newCanvasWidth = 16*pixelWidth;
-canvas.style.width = newCanvasWidth+"px";
-let newCanvasHeight = 16*pixelHeight;
-canvas.style.height = newCanvasHeight+"px";
-console.log("Width: " + canvas.style.width + " Height: " + canvas.style.height);
 
-let stringStyle = "width:" + pixelWidth + "px; height: " + pixelHeight + "px;";
-// add them to canvas
-for (let i = 0; i < numberOfPixels; i++) {
-    const pixel = document.createElement("div");
-    pixel.setAttribute("style", stringStyle);
-    pixel.addEventListener("mouseover", changeColor);
-    canvas.appendChild(pixel);
+const LOW_RES = 16;
+const MID_RES = 32;
+const HIGH_RES = 64;
+create_pixels(MID_RES);
+
+const lowResButton = document.getElementById("low");
+lowResButton.addEventListener("click", function(){
+    deletePixels();
+    create_pixels(LOW_RES);
+})
+const midResButton = document.getElementById("medium");
+midResButton.addEventListener("click", function(){
+    deletePixels();
+    create_pixels(MID_RES);
+})
+const highResButton = document.getElementById("High");
+highResButton.addEventListener("click", function(){
+    deletePixels();
+    create_pixels(HIGH_RES);
+})
+
+function deletePixels(){
+    let pixels = Array.from(document.getElementsByClassName("pixel"));
+    pixels.forEach(element => {
+        element.remove();
+    });
+}
+
+function create_pixels(res) {
+    let numberOfVerticalPixels = res;
+    // make the pixels square
+    // get the height of the pixels
+    let pixelHeight = Math.floor(canvasHeight / res);
+    let numberOfHorizontalPixels = Math.floor(canvasWidth / pixelHeight);
+    let pixelWidth = pixelHeight;
+
+    // calculate number of pixels needed to fill the canvas 
+    let numberOfPixels = numberOfHorizontalPixels * numberOfVerticalPixels;
+    let newCanvasWidth = numberOfHorizontalPixels * pixelWidth;
+    canvas.style.width = newCanvasWidth + "px";
+    let newCanvasHeight = numberOfVerticalPixels * pixelHeight;
+    canvas.style.height = newCanvasHeight + "px";
+
+    let stringStyle = "width:" + pixelWidth + "px; height: " + pixelHeight + "px;";
+    // add them to canvas
+    for (let i = 0; i < numberOfPixels; i++) {
+        const pixel = document.createElement("div");
+        pixel.classList.add("pixel")
+        pixel.setAttribute("style", stringStyle);
+        pixel.addEventListener("mouseover", changeColor);
+        canvas.appendChild(pixel);
+    }
 }
 
 // Program the hold and color feature
@@ -34,12 +66,10 @@ let down = false;
 canvas.addEventListener("mousedown", mousedown);
 function mousedown(){
     down = true;
-    console.log("Mouse Down");
 }
 canvas.addEventListener("mouseup", mouseup);
 function mouseup(){
     down = false;
-    console.log("Mouse Up");
 }
 
 function changeColor(e){
